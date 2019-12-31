5ca3a8a02bda344496d72e836b653a4ee0925d1a
Jeff Epler <jepler@gmail.com>
2019-12-31 11:07:29 -0600

    Clean up comments and dead code
    .. this involves inverting one condition, since the branch that needed
    to be preserved was the 'else' branch

--- samd/dma.c
+++ samd/dma.c
@@ -110,7 +110,6 @@ static int32_t shared_dma_transfer(void* peripheral,
     } else {
     #endif
 
-        // sercom index is incorrect for SAMD51
         dma_configure(SHARED_TX_CHANNEL, sercom_index(peripheral) * 2 + FIRST_SERCOM_TX_TRIGSRC, false);
         tx_active = true;
         if (buffer_in != NULL) {
@@ -155,8 +154,6 @@ static int32_t shared_dma_transfer(void* peripheral,
     if (sercom) {
         SercomSpi *s = &((Sercom*) peripheral)->SPI;
         s->INTFLAG.reg = SERCOM_SPI_INTFLAG_RXC | SERCOM_SPI_INTFLAG_DRE;
-    } else {
-        //QSPI->INTFLAG.reg = QSPI_INTFLAG_RXC | QSPI_INTFLAG_DRE;
     }
     // Start the RX job first so we don't miss the first byte. The TX job clocks
     // the output.
@@ -168,19 +165,10 @@ static int32_t shared_dma_transfer(void* peripheral,
     }
 
 
-    if (sercom) {
-        //DMAC->SWTRIGCTRL.reg |= (1 << SHARED_TX_CHANNEL);
-    } else {
-        // Do a manual copy to trigger then DMA. We do 32-bit accesses to match the DMA.
-        #pragma GCC diagnostic push
-        #pragma GCC diagnostic ignored "-Wcast-align"
+    if (!sercom) {
         if (rx_active) {
-            //buffer_in[0] = *src;
             DMAC->SWTRIGCTRL.reg |= (1 << SHARED_RX_CHANNEL);
-        } else {
-            //*(uint32_t*)dest = ((uint32_t*) buffer_out)[0];
         }
-        #pragma GCC diagnostic pop
     }
 
     #ifdef SAMD51
@@ -211,9 +199,7 @@ static int32_t shared_dma_transfer(void* peripheral,
     }
     #endif
 
-    // Channels cycle between Suspend -> Pending -> Busy and back while transfering. So, we check
-    // the channels transfer status for an error or completion.
-    // (However, the flags being checked here are actually CHINTFLAG "complete" and "error")
+    // busy-wait for the RX and TX DMAs to either complete or encounter an error
     if (rx_active) {
         while ((dma_transfer_status(SHARED_RX_CHANNEL) & 0x3) == 0) {}
     }
